cb402929302a7d308df493d39eadd810f654481b
jinbao4 <random@example.com>
2025-06-07 23:19:03 +0100

    Add missing components and libraries for deployment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,9 +29,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <SpeedInsights>
-          {children}
-        </SpeedInsights>
+        {children}
+        <SpeedInsights /> 
       </body>
     </html>
   );
